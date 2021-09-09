3b8ad45d4a9601c2ff017b1ad1e5ba0a494da2e7
Technicalcourses2021 <85402649+Technicalcourses2021@users.noreply.github.com>
2021-09-09 21:03:09 +0530

    Update BaseClass.java

--- src/main/java/com/base/BaseClass.java
+++ src/main/java/com/base/BaseClass.java
@@ -17,7 +17,7 @@ public class BaseClass {
 		if (System.getProperty("browser").equalsIgnoreCase("Chrome")) {
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
-		} else if (System.getProperty("browser").equalsIgnoreCase("Firefox")) {
+		} else if (System.getProperty("browser").equalsIgnoreCase("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
 		} else if (System.getProperty("browser").equalsIgnoreCase("IE")) {
